ed8e047321bc3c44d60855cc27fa428ed1177818
mike <fantazuor@gmail.com>
2015-08-27 16:25:58 +0300

    add readme

--- src/main/java/com/github/truerss/ContentExtractor.java
+++ src/main/java/com/github/truerss/ContentExtractor.java
@@ -2,7 +2,6 @@ package com.github.truerss;
 
 import org.jsoup.nodes.Element;
 
-import java.nio.channels.Selector;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
