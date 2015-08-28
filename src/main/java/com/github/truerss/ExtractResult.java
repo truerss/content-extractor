1f5111b57bd2f70f375079a1d2c641f5463be7b4
mike <fantazuor@gmail.com>
2015-08-28 15:53:32 +0300

    update access for extract result

--- src/main/java/com/github/truerss/ExtractResult.java
+++ src/main/java/com/github/truerss/ExtractResult.java
@@ -4,7 +4,7 @@ package com.github.truerss;
  * Created by mike on 27.8.15.
  */
 public class ExtractResult {
-  final String selector;
+  public final String selector;
   public ExtractResult(String selector) {
     this.selector = selector;
   }
