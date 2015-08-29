bf8c1045eb507d4145da85c3433b891944724a6a
mike <fantazuor@gmail.com>
2015-08-29 11:55:23 +0300

    refactorign code

--- src/main/java/com/github/truerss/ContentExtractor.java
+++ src/main/java/com/github/truerss/ContentExtractor.java
@@ -3,94 +3,37 @@ package com.github.truerss;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.*;
 /**
  * Created by mike on 27.8.15.
  */
 public class ContentExtractor {
 
-  private static String[] forRemove = {"a", "ul", "code", "pre", "form", "input"};
-
   public static ExtractResult extract(Element element) {
-    Arrays.stream(forRemove)
-        .forEach(tag -> element.select(tag)
-            .forEach(x -> x.remove()));
-
-    String result = null;
-
-    Elements article = element.select("article");
-    if (article.size() == 1) {
-      result = article.first().cssSelector();
+    Elements articles = element.select("article");
+    if (articles.size() == 1) {
+      return new ExtractResult(articles.first().cssSelector());
     }
 
-    ElementExt body = new ElementExt(element);
-
-    TNode root = new TNode("root", Types.anotherType);
-
-    NodeX.extract(body).stream().map(z ->
-            Arrays.stream(z.path.split(" > "))
-                .map(x -> new TNode(x, z.tpe))
-                .collect(Collectors.toCollection(ArrayList::new))
-    ).forEach(x -> root.addChild0(x));
-
-    TNode[] tnodeStream = root.all().stream()
-        .filter(x -> x.all().stream().allMatch(c -> c.size() > 0))
-        .filter(x -> x.childrenCount() >= 1)
-        .filter(x -> x.size() > 0)
-        .filter(x -> x.size().equals(x.childrenSize()))
-        .toArray(TNode[]::new);
-
-    if (tnodeStream.length == 0) {
-      throw new RuntimeException("Empty array of elements from " + element.cssSelector() + " selector");
-    }
-
-    Optional<TNode> optTNode = Arrays.stream(tnodeStream)
-        .parallel().max(Comparator.comparing(x -> x.size()));
-
-    String need1 = optTNode.get().data;
-    Optional<Element> e1 = element.select(need1).stream()
-        .max(Comparator.comparingInt(x -> x.text().length()));
-
-    String need2 = tnodeStream[0].data;
-    Optional<Element> e2 = element.select(need2).stream()
-        .max(Comparator.comparingInt(x -> x.text().length()));
-
-    if (result == null) {
-      if (e1.isPresent() && e2.isPresent()) {
-        if (e1.isPresent() && !e2.isPresent()) {
-          result = e1.get().cssSelector();
-        } else if (e2.isPresent() && !e1.isPresent()) {
-          result = e2.get().cssSelector();
-        } else {
-          if (e1.get().text().length() > e2.get().text().length()) {
-            result = e1.get().cssSelector();
+    HashMap<String, Long> map = new HashMap<>();
+    Elements elements = element.select("p:matchesOwn(^.+$), div:matchesOwn(^.+$)");
+    elements.stream()
+        .forEach(x -> {
+          String key = x.parent().cssSelector();
+          Long v = map.get(key);
+          if (v == null) {
+            map.put(key, (long)x.text().length());
           } else {
-            result = e2.get().cssSelector();
+            map.put(key, x.text().length() + v);
           }
-        }
-      } else {
-        throw new RuntimeException("Fail extract content from " + element.cssSelector());
-      }
-    }
-    // and need remove html if it first element
-    if (result.startsWith("html")) {
-      StringBuilder bf = new StringBuilder(result.length() - 4);
-      String[] tmp = result.split(" > ");
-      int length = tmp.length;
-      for (int i = 1; i < length; i ++) {
-        bf.append(tmp[i]);
-        if (i != length-1) {
-          bf.append(" > ");
-        }
-      }
-      result = bf.toString();
-    }
+        });
+
+    String result = map.entrySet()
+        .stream().max((entry1, entry2) ->
+            entry1.getValue() > entry2.getValue() ? 1 : -1).get().getKey();
 
     return new ExtractResult(result);
+
   }
 
 }
